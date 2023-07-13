bb51869d43b86a673d9507c099e1a017d54c6cb2
Sandy Carter <bwrsandman@gmail.com>
2023-07-13 19:49:52 -0400

    Add test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <spdlog/spdlog.h>
 
-void hello()
+int main(int argc, char* argv[])
 {
-    spdlog::info("Hello world!");
+    #if defined(__i386__) || defined(_M_IX86)
+    spdlog::info("Architecture: x86 (32-bit)");
+    #elif defined(__x86_64__) || defined(_M_X64)
+    spdlog::info("Architecture: x64 (64-bit)");
+    #elif defined(__arm__)
+    spdlog::info("Architecture: ARM (32-bit)");
+    #elif defined(__aarch64__)
+    spdlog::info("Architecture: ARM (64-bit)");
+    #else
+    spdlog::info("Architecture: Unknown");
+    #endif
+    return 1;
 }

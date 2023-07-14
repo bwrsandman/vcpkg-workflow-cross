396389831bb2dbab08c07b9e0ef2e5dcd4fad038
Sandy Carter <bwrsandman@gmail.com>
2023-07-14 15:43:40 -0400

    test bin2c

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+#include <cstdint>
 #include <spdlog/spdlog.h>
 #include <bx/bx.h>
+
+#include "binary.h"
 
 int main(int argc, char* argv[])
 {
     spdlog::info("Cpu: {}, Arch: {}, CRT: {}, CPP: {}, Platform: {}", BX_CPU_NAME, BX_ARCH_NAME, BX_CRT_NAME, BX_CPP_NAME, BX_PLATFORM_NAME);
+    spdlog::info("binary contents: {}", binary_data[0]);
     return 0;
 }

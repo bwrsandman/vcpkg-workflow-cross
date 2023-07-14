b048719b06e07396c32e0990427b3b069550b89f
Sandy Carter <bwrsandman@gmail.com>
2023-07-14 01:37:47 -0400

    use bgfx::bx

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 #include <spdlog/spdlog.h>
+#include <bx/bx.h>
 
 int main(int argc, char* argv[])
 {
-    #if defined(__i386__) || defined(_M_IX86)
-    spdlog::info("Architecture: x86 (32-bit)");
-    #elif defined(__x86_64__) || defined(_M_X64)
-    spdlog::info("Architecture: x64 (64-bit)");
-    #elif defined(__arm__)
-    spdlog::info("Architecture: ARM (32-bit)");
-    #elif defined(__aarch64__)
-    spdlog::info("Architecture: ARM (64-bit)");
-    #else
-    spdlog::info("Architecture: Unknown");
-    #endif
+    spdlog::info("Cpu: {}, Arch: {}, CRT: {}, CPP: {}, Platform: {}", BX_CPU_NAME, BX_ARCH_NAME, BX_CRT_NAME, BX_CPP_NAME, BX_PLATFORM_NAME);
     return 1;
 }

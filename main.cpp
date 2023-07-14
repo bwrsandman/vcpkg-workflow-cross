992c0bf6815c5ffcc9f1e5ed894bacb9a41f94cd
Sandy Carter <bwrsandman@gmail.com>
2023-07-14 09:49:37 -0400

    always

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@
 int main(int argc, char* argv[])
 {
     spdlog::info("Cpu: {}, Arch: {}, CRT: {}, CPP: {}, Platform: {}", BX_CPU_NAME, BX_ARCH_NAME, BX_CRT_NAME, BX_CPP_NAME, BX_PLATFORM_NAME);
-    return 1;
+    return 0;
 }

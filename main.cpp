2e74f9ddee85b281166c531f263b15033a35c883
Sandy <bwrsandman@gmail.com>
2023-07-15 00:27:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <cstdint>
 #include <spdlog/spdlog.h>
 #include <bx/bx.h>
-
+#include <bgfx/bgfx.h>
 #include "binary.h"
 
 int main(int argc, char* argv[])
 {
+    bgfx::frame();
     spdlog::info("Cpu: {}, Arch: {}, CRT: {}, CPP: {}, Platform: {}", BX_CPU_NAME, BX_ARCH_NAME, BX_CRT_NAME, BX_CPP_NAME, BX_PLATFORM_NAME);
     spdlog::info("binary contents: {}", binary_data[0]);
     return 0;

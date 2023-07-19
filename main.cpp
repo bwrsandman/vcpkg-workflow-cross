ef2fd79d9a5fb2deec17aa6e845e51f03b60ca5f
Sandy Carter <bwrsandman@gmail.com>
2023-07-18 21:57:36 -0400

    test openal

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <cstdint>
 #include <spdlog/spdlog.h>
-#include <bx/bx.h>
-#include <bgfx/bgfx.h>
+#include <AL/al.h>
 
 int main(int argc, char* argv[])
 {
-    bgfx::frame();
-    spdlog::info("Cpu: {}, Arch: {}, CRT: {}, CPP: {}, Platform: {}", BX_CPU_NAME, BX_ARCH_NAME, BX_CRT_NAME, BX_CPP_NAME, BX_PLATFORM_NAME);
+    spdlog::info("{}", alGetString(AL_FORMAT_MONO8));
     return 0;
 }
